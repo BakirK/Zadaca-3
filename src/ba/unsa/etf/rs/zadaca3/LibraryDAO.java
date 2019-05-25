24e70ea3181088321defa9c2e77d85f36358cbc2
Bakir Karovic <bakir.karovic@gmail.com>
2019-05-25 16:23:24 +0200

    Dodana metoda loadBooks koja puni listu podacima iz baze i postavlja trenutnu knjigu na prvu u listi

--- src/ba/unsa/etf/rs/zadaca3/LibraryDAO.java
+++ src/ba/unsa/etf/rs/zadaca3/LibraryDAO.java
@@ -62,6 +62,7 @@ public class LibraryDAO {
     //TODO: metoda za punjenje liste i prepared statements
     public LibraryDAO() {
         prepareStatements();
+        loadBooks();
     }
 
 
@@ -83,6 +84,8 @@ public class LibraryDAO {
                     "publishdate = ? WHERE id = ?; COMMIT");
 
             deleteAllBooksStatement = conn.prepareStatement("DELETE FROM books WHERE 1 = 1; COMMIT;");
+
+            getBooksStatement = conn.prepareStatement("SELECT id, author, title, isbn, pagecount, publishdate FROM books;");
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -150,6 +153,22 @@ public class LibraryDAO {
         addBook(new Book("Meša Selimović", "Tvrđava", "abcd", 500, LocalDate.now()));
         addBook(new Book("Ivo Andrić", "Travnička hronika", "abcd", 500, LocalDate.now()));
         addBook(new Book("J. K. Rowling", "Harry Potter", "abcd", 500, LocalDate.now()));
+    }
+
+    public void loadBooks() {
+        try {
+            ResultSet set = getBooksStatement.executeQuery();
+            while(set.next()) {
+                books.add(new Book(set.getInt(1), set.getString(2),
+                        set.getString(3), set.getString(4),
+                        set.getInt(5), set.getDate(6).toLocalDate()));
+            }
+            if (books.size() > 0) {
+                currentBook.set(books.get(0));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
