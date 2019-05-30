87487fd69a712997a4f4a509fa5f49de94c283e4
Bakir Karovic <bakir.karovic@gmail.com>
2019-05-31 00:28:24 +0200

    Implementirana klasa XMLFormat

---
+++ src/ba/unsa/etf/rs/zadaca3/XMLFormat.java
@@ -0,0 +1,56 @@
+package ba.unsa.etf.rs.zadaca3;
+
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+public class XMLFormat {
+    public static ArrayList<Book> read(File file) throws FileNotFoundException {
+        ArrayList<Book> books = new ArrayList<>();
+        Book book = null;
+        XMLDecoder input = null;
+        try {
+            input = new XMLDecoder(new FileInputStream("Data.xml"));
+            while((book = (Book) input.readObject()) != null) {
+                books.add(book);
+            }
+            input.close();
+        } catch(Exception e) {
+            input.close();
+            System.out.println("Greška: "+e);
+            throw new FileNotFoundException();
+        }
+        return books;
+    }
+
+
+    public static void write(File file, ArrayList<Book> books) throws FileNotFoundException {
+        XMLEncoder output = null;
+        try {
+            output = new XMLEncoder(new FileOutputStream(file.getAbsolutePath()));
+            /*output.setPersistenceDelegate(LocalDate.class,
+                new PersistenceDelegate() {
+                    @Override
+                    protected Expression instantiate(Object localDate, Encoder encdr) {
+                        return new Expression(localDate,
+                                LocalDate.class,
+                                "parse",
+                                new Object[]{localDate.toString()});
+                    }
+                });*/
+            for (Book book: books) {
+                output.writeObject(book);
+            }
+            output.close();
+        } catch(Exception e) {
+            output.close();
+            System.out.println("Greška: "+e);
+            throw new FileNotFoundException();
+        }
+    }
+
+}
